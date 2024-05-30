d6117ddf164d755fd4b35967efcf6c136bdc591a
lucas-faget <lucas.faget@laposte.net>
2024-05-30 12:36:56 +0200

    Nuxt introduction

--- app.vue
+++ app.vue
@@ -1,5 +1,26 @@
+<script setup lang="ts">
+// Private or public tokens that need to be specified after build using environment variables.
+const runtimeConfig = useRuntimeConfig();
+// Public tokens that are determined at build time, website configuration such as theme variant, title and any project config that are not sensitive.
+const appConfig = useAppConfig();
+</script>
+
 <template>
     <div>
-        <NuxtWelcome />
+        <NuxtLayout>
+            <NuxtPage />
+        </NuxtLayout>
     </div>
 </template>
+
+<style>
+.page-enter-active,
+.page-leave-active {
+    transition: all 0.4s;
+}
+.page-enter-from,
+.page-leave-to {
+    opacity: 0;
+    filter: blur(1rem);
+}
+</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,24 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+    css: ["~/assets/css/main.css"],
+    app: {
+        pageTransition: { name: "page", mode: "out-in" },
+    },
     devtools: { enabled: true },
+    $production: {
+        routeRules: {
+            "/**": { isr: true },
+        },
+    },
+    $development: {
+        //
+    },
+    runtimeConfig: {
+        // The private keys which are only available server-side
+        apiSecret: "123",
+        // Keys within public are also exposed client-side
+        public: {
+            apiBase: "/api",
+        },
+    },
 });
