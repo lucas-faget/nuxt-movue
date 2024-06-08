5f09068e06029929af375fc2eb9ef0f941bf861c
lucas-faget <lucas.faget@laposte.net>
2024-06-09 01:59:06 +0200

    Adding default poster image, adding link to movie page when clicking the poster, adding details about movie on movie page

--- components/MovieList.vue
+++ components/MovieList.vue
@@ -1,27 +1,33 @@
 <script setup lang="ts">
+import { format } from "date-fns";
 import type { Movie } from "~/types/Movie";
 import { getPosterUrl } from "~/services/movies";
 import { PosterSize } from "~/types/PosterSize";
 
-defineProps<{
+const props = defineProps<{
     movies: Movie[];
 }>();
 </script>
 
 <template>
-    <div class="flex flex-wrap gap-4">
-        <div class="w-40 flex flex-col gap-2" v-for="movie in movies" :key="movie.id">
-            <div>
-                <img :src="getPosterUrl(PosterSize.W154, movie.poster_path)" :alt="movie.title" />
-            </div>
-            <div>
-                <p>{{ movie.title }}</p>
-            </div>
-            <div>
-                <NuxtLink :to="`/movies/${movie.id}`">
-                    <UButton color="white" variant="solid">Details</UButton>
-                </NuxtLink>
-            </div>
-        </div>
+    <div class="flex flex-wrap gap-6">
+        <template v-for="movie in movies" :key="movie.id">
+            <NuxtLink :to="`/movies/${movie.id}`">
+                <div class="w-40 flex flex-col gap-2 cursor-pointer">
+                    <div>
+                        <img
+                            class="w-full"
+                            :src="getPosterUrl(PosterSize.W154, movie.poster_path)"
+                            :alt="movie.title"
+                            onerror="this.onerror = null; this.src='poster-not-found.jpg'"
+                        />
+                    </div>
+                    <p v-if="movie.title">{{ movie.title }}</p>
+                    <p v-if="movie.release_date" class="text-sm text-gray-400">
+                        {{ format(movie.release_date, "d MMM yyyy") }}
+                    </p>
+                </div>
+            </NuxtLink>
+        </template>
     </div>
 </template>
--- pages/movies/[id].vue
+++ pages/movies/[id].vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { ref } from "vue";
 import { useRoute } from "vue-router";
+import { format } from "date-fns";
 import type { Movie } from "~/types/Movie";
 import type { MovieList } from "~/types/MovieList";
 import { BackdropSize } from "~/types/BackdropSize";
@@ -9,14 +10,14 @@ import { fetchMovieDetails, fetchSimilarMovies, getBackdropUrl } from "~/service
 const route = useRoute();
 
 const id: string = route.params.id as string;
-const movie = ref<Movie | undefined>(undefined);
+const movie = ref<any | undefined>(undefined);
 const similarMovies = ref<Movie[]>([]);
 
 const isSimilarMovieListEmpty = computed(() => similarMovies.value.length === 0);
 
 async function getMovieById() {
     try {
-        const data: Movie = (await fetchMovieDetails(id)) as Movie;
+        const data: any = (await fetchMovieDetails(id)) as any;
         movie.value = data;
     } catch (error) {
         console.error("Failed to fetch movie: ", error);
@@ -37,16 +38,67 @@ getSimilarMovies();
 </script>
 
 <template>
-    <section v-if="movie" class="w-full mt-16 flex flex-col gap-8">
+    <section v-if="movie" class="w-full mt-16 flex flex-col">
         <div>
             <div class="flex">
-                <div class="basis-2/5 p-16 flex flex-col gap-8">
-                    <p class="text-5xl font-title">{{ movie.title }}</p>
-                    <UDivider />
-                    <p>{{ movie.overview }}</p>
-                    <UDivider />
-                    <p>{{ movie.release_date }}</p>
-                    <p>{{ movie.vote_average }}</p>
+                <div class="basis-2/5 p-12 flex flex-col gap-4">
+                    <p v-if="movie.title" class="text-6xl font-title">{{ movie.title }}</p>
+
+                    <UDivider v-if="movie.title" />
+
+                    <div
+                        v-if="movie.release_date || movie.runtime"
+                        class="flex justify-between items-center leading-none"
+                    >
+                        <div v-if="movie.release_date" class="flex flex-col items-center">
+                            <span class="text-4xl font-title">{{
+                                format(movie.release_date, "yyyy")
+                            }}</span>
+                            <span class="text-md text-gray-400">{{
+                                format(movie.release_date, "d MMM")
+                            }}</span>
+                        </div>
+                        <div v-if="movie.runtime">
+                            <span class="text-3xl font-title">{{ movie.runtime }}</span>
+                            <span class="text-md text-gray-400 ml-1">min</span>
+                        </div>
+                    </div>
+
+                    <UDivider v-if="movie.release_date || movie.runtime" />
+
+                    <div
+                        v-if="movie.genres && movie.genres.length > 0"
+                        class="flex gap-2 items-center"
+                    >
+                        <UBadge
+                            v-for="genre in movie.genres"
+                            size="lg"
+                            color="white"
+                            variant="solid"
+                        >
+                            {{ genre.name }}
+                        </UBadge>
+                    </div>
+
+                    <UDivider v-if="movie.genres && movie.genres.length > 0" />
+
+                    <p v-if="movie.overview" class="text-gray-400">{{ movie.overview }}</p>
+
+                    <UDivider v-if="movie.overview" />
+
+                    <div
+                        v-if="movie.vote_average || movie.origin_country"
+                        class="flex justify-between items-center"
+                    >
+                        <div v-if="movie.vote_average" class="flex gap-2 items-center">
+                            <UIcon class="w-6 h-6" name="i-heroicons-star" />
+                            <span>{{ movie.vote_average.toFixed(1) }}</span>
+                        </div>
+                        <div v-if="movie.origin_country" class="flex gap-2 items-center">
+                            <UIcon class="w-6 h-6" name="i-heroicons-globe-americas" />
+                            <UKbd size="md">{{ movie.origin_country[0] }}</UKbd>
+                        </div>
+                    </div>
                 </div>
                 <div class="basis-3/5">
                     <div class="image-container">
