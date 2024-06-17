37fb31eedc5329173eb7d1ed6f26e8ac21399640
lucas-faget <lucas.faget@laposte.net>
2024-06-17 12:38:23 +0200

    On the movie carousel add the title and the date when hovering with the mouse

--- components/MovieCarousel.vue
+++ components/MovieCarousel.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import { format } from "date-fns";
 import type { Movie } from "~/types/Movie";
 const { $images } = useNuxtApp() as any;
 
@@ -10,31 +11,54 @@ defineProps<{
 <template>
     <div>
         <UCarousel v-slot="{ item: movie }" :items="movies" arrows>
-            <div class="relative mt-5">
-                <UBadge
-                    v-if="movie.vote_average"
-                    size="lg"
-                    color="amber"
-                    class="badge-center absolute bg-amber-200 font-bold"
-                    :ui="{ rounded: 'rounded-full' }"
-                >
-                    {{ movie.vote_average.toFixed(1) }}
-                </UBadge>
-                <img
-                    :src="$images.getPosterUrl(movie.poster_path)"
-                    width="200"
-                    height="300"
-                    draggable="false"
-                />
-            </div>
+            <NuxtLink :to="`/movies/${movie.id}`">
+                <div class="movie relative mt-5">
+                    <div class="badge absolute z-10">
+                        <UBadge
+                            v-if="movie.vote_average"
+                            size="lg"
+                            color="amber"
+                            class="font-bold bg-amber-200"
+                            :ui="{ rounded: 'rounded-full' }"
+                        >
+                            {{ movie.vote_average.toFixed(1) }}
+                        </UBadge>
+                    </div>
+                    <div class="relative overflow-hidden">
+                        <div
+                            class="movie-details absolute bottom-0 w-full p-2 flex flex-col gap-1 bg-slate-800"
+                        >
+                            <div class="text-lg font-title">{{ movie.title }}</div>
+                            <div class="font-title text-gray-400">
+                                {{ format(movie.release_date, "d MMM yyyy") }}
+                            </div>
+                        </div>
+                        <img
+                            :src="$images.getPosterUrl(movie.poster_path)"
+                            width="200"
+                            height="300"
+                            draggable="false"
+                        />
+                    </div>
+                </div>
+            </NuxtLink>
         </UCarousel>
     </div>
 </template>
 
 <style scoped>
-.badge-center {
+.badge {
     top: 0;
     left: 50%;
     transform: translate(-50%, -50%);
+}
+
+.movie-details {
+    transform: translateY(100%);
+    transition: transform 0.3s ease;
+}
+
+.movie:hover .movie-details {
+    transform: translateY(0%);
 }
 </style>
